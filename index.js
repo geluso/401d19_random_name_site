8499eee3390aa1fd531d44156e07054ecf176c5d
Steve Geluso <stevegeluso@gmail.com>
2017-12-28 12:06:17 -0800

    centering and coloring so good.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.get('/', (req, res) => {
   var name = randomPerson.randomName();
-  res.send(name);
+  res.send('<style>body{background-color:red; text-align: center;}</style>' + name);
 });
 
 var port = process.env.PORT || 3000;
